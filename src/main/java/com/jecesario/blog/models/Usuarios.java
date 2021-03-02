936726e9a0faf1460a988efb936ee6c102bdba47
Jecesario <jp@soloid.com.br>
2021-03-02 12:12:16 -0300

    chore(models): criando model de usuarios

---
+++ src/main/java/com/jecesario/blog/models/Usuarios.java
@@ -0,0 +1,32 @@
+package com.jecesario.blog.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table
+public class Usuarios {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String usuario;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String senha;
+
+	
+}
