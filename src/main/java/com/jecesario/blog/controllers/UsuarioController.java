fa2c82930aa04c3e5a124840be0150be5b8b699c
Jecesario <jp@soloid.com.br>
2021-03-02 14:33:39 -0300

    fear(controller): criando endpoint para logar

--- src/main/java/com/jecesario/blog/controllers/UsuarioController.java
+++ src/main/java/com/jecesario/blog/controllers/UsuarioController.java
@@ -1,10 +1,17 @@
 package com.jecesario.blog.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jecesario.blog.models.UsuarioLogin;
 import com.jecesario.blog.service.UsuarioService;
 
 @RestController
@@ -15,5 +22,9 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService service;
 	
-	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> logar(@RequestBody Optional<UsuarioLogin> user) {
+		return service.logar(user).map(obj -> ResponseEntity.ok(obj))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
 }
