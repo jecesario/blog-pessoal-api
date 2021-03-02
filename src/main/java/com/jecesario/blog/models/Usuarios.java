5d467a72d07b8d440cc50e63626254eae4b4b233
Jecesario <jp@soloid.com.br>
2021-03-02 12:14:29 -0300

    chore(models): criando model usuarioslogin

--- src/main/java/com/jecesario/blog/models/Usuarios.java
+++ src/main/java/com/jecesario/blog/models/Usuarios.java
@@ -15,18 +15,49 @@ public class Usuarios {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 2, max = 100)
 	private String nome;
-	
+
 	@NotNull
 	@Size(min = 2, max = 100)
 	private String usuario;
-	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String senha;
 
-	
 }
